e4f2f5ce54366eb98750326a4545c54d6ba81727
江波波 <mvcome@163.com>
2016-10-22 19:16:08 +0800

    ss
    ss

---
+++ PhantomJSCrawler/phantomJs.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace PhantomJSCrawler
+{
+    partial class Mainform
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Mainform));
+            this.startBtn = new System.Windows.Forms.Button();
+            this.urlTextBox = new System.Windows.Forms.TextBox();
+            this.logTextBox = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // startBtn
+            // 
+            this.startBtn.Location = new System.Drawing.Point(358, 6);
+            this.startBtn.Name = "startBtn";
+            this.startBtn.Size = new System.Drawing.Size(64, 31);
+            this.startBtn.TabIndex = 0;
+            this.startBtn.Text = "开始";
+            this.startBtn.UseVisualStyleBackColor = true;
+            this.startBtn.Click += new System.EventHandler(this.startBtn_Click);
+            // 
+            // urlTextBox
+            // 
+            this.urlTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.urlTextBox.Location = new System.Drawing.Point(12, 12);
+            this.urlTextBox.Name = "urlTextBox";
+            this.urlTextBox.Size = new System.Drawing.Size(340, 21);
+            this.urlTextBox.TabIndex = 1;
+            this.urlTextBox.Text = "http://localhost";
+            // 
+            // logTextBox
+            // 
+            this.logTextBox.BackColor = System.Drawing.SystemColors.WindowText;
+            this.logTextBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.logTextBox.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.logTextBox.ForeColor = System.Drawing.Color.Gold;
+            this.logTextBox.Location = new System.Drawing.Point(12, 39);
+            this.logTextBox.Name = "logTextBox";
+            this.logTextBox.Size = new System.Drawing.Size(410, 166);
+            this.logTextBox.TabIndex = 2;
+            this.logTextBox.Text = "";
+            this.logTextBox.LinkClicked += new System.Windows.Forms.LinkClickedEventHandler(this.logTextBox_LinkClicked);
+            // 
+            // Mainform
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(434, 213);
+            this.Controls.Add(this.logTextBox);
+            this.Controls.Add(this.urlTextBox);
+            this.Controls.Add(this.startBtn);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Mainform";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "phantomJsCrawler    --By jarbob";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Mainform_FormClosing_1);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button startBtn;
+        private System.Windows.Forms.TextBox urlTextBox;
+        private System.Windows.Forms.RichTextBox logTextBox;
+    }
+}
+
---
+++ PhantomJSCrawler/phantomJs.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.PhantomJS;
+using OpenQA.Selenium.Chrome;
+using System.Collections.ObjectModel;
+using System.Threading;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+using OpenQA.Selenium.Remote;
+using NUnit;
+using OpenQA.Selenium.Firefox;
+
+namespace PhantomJSCrawler
+{
+    public partial class Mainform : Form
+    {
+        public Mainform()
+        {
+            InitializeComponent();
+        }
+
+        private void startBtn_Click(object sender, EventArgs e)
+        {
+
+            Control.CheckForIllegalCrossThreadCalls = false;
+            Thread s = new Thread(new ParameterizedThreadStart(GetIframeResult));
+            s.IsBackground = true;
+            s.Start(urlTextBox.Text);
+            Console.WriteLine("线程已启动");
+            //while (Process.GetProcessesByName("PhantomJS").Length > 0)
+            //{
+
+            //    WindowChange.ToChange(Process.GetProcessesByName("PhantomJS")[0].MainWindowHandle, false);
+
+            //}
+
+
+        }
+
+
+        private void Log(string detail)
+        {
+            this.logTextBox.AppendText(detail + "\r\n");
+        }
+
+        private void GetIframeResult(object uri)
+        {
+
+            //IWebDriver driver = new ChromeDriver();+
+            /*phantomjs*/
+            //var phantomJSDriverService = PhantomJSDriverService.CreateDefaultService();
+            //phantomJSDriverService.ProxyType = "none";
+            //PhantomJSOptions options = new PhantomJSOptions();
+            ////options.AddAdditionalCapability("cssSelectorsEnabled",false);
+            //IWebDriver driver = new PhantomJSDriver(phantomJSDriverService);
+
+            /*firefox*/
+
+            //IWebDriver driver = new FirefoxDriver();
+
+            /*chrome*/
+            ChromeDriverService cds = ChromeDriverService.CreateDefaultService();
+            cds.HideCommandPromptWindow = true;
+            IWebDriver driver = new ChromeDriver(cds);
+
+
+
+            driver.Navigate().GoToUrl(uri as string);
+            //string pageInfo = driver.PageSource;
+           
+            //var driver = new PhantomJSDriver
+            //{
+            //    Url = uri as string,
+            //};
+
+            //driver.Navigate();
+
+            //var source = driver.PageSource;
+            ReadOnlyCollection<IWebElement> elem = driver.FindElements(By.TagName("iframe"));
+            foreach (IWebElement item in elem)
+            {
+                Log(item.GetAttribute("src"));
+
+            }
+           // Screenshot ss = driver.GetScreenshot();
+            //ss.SaveAsFile(driver.Title + ".png", System.Drawing.Imaging.ImageFormat.Png);
+            driver.Close();
+            driver.Dispose();
+            driver.Quit();
+
+
+        }
+
+        private void logTextBox_LinkClicked(object sender, LinkClickedEventArgs e)
+        {
+            System.Diagnostics.Process.Start(e.LinkText);
+        }
+
+
+        private void Mainform_FormClosing_1(object sender, FormClosingEventArgs e)
+        {
+            System.Diagnostics.Process.GetCurrentProcess().Kill();
+        }
+
+
+
+    }
+
+
+    public class WindowChange
+    {
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindowAsync(IntPtr hWnd, int nCmdShow);
+
+        private const int SW_HIDE = 0;          //常量，隐藏
+        private const int SW_SHOWNORMAL = 1;    //常量，显示，标准状态
+        private const int SW_SHOWMINIMIZED = 2; //常量，显示，最小化
+        private const int SW_SHOWMAXIMIZED = 3; //常量，显示，最大化
+        private const int SW_SHOWNOACTIVATE = 4;//常量，显示，不激活
+        private const int SW_RESTORE = 9;       //常量，显示，回复原状
+        private const int SW_SHOWDEFAULT = 10;  //常量，显示，默认
+
+        public static void ToChange(IntPtr p, bool isboolean)
+        {
+            if (isboolean)
+            {
+                ShowWindowAsync(p, SW_SHOWNORMAL);
+            }
+            else
+            {
+                ShowWindowAsync(p, SW_HIDE);
+            }
+        }
+    }
+}
